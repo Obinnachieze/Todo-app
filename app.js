94b1dd54f75701a70c1a8643c8cc1afa6b9089e5
Obinnachieze <wisdomchieze0@gmail.com>
2026-01-10 23:01:38 +0100

    added logic

---
+++ app.js
@@ -0,0 +1,10 @@
+const todo = () => {
+  document.body.innerHTML = `
+     <div id="container">
+      <input type="text" />
+      <label for="text"></label>
+      <button>ADD</button>
+    </div>
+  `;
+};
+todo();
