d51a8ca7cea5093e8a884b1316167a458d188c6f
Obinnachieze <wisdomchieze0@gmail.com>
2026-01-10 23:33:48 +0100

    new

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const todo = () => {
+const todo = (button) => {
   document.body.innerHTML = `
      <div id="container">
-      <input type="text" />
-      <label for="text"></label>
-      <button>ADD</button>
+     <h1>Todo List</h1>
+      <input type="text" id="todo-input" />
+      <label for="todo-input"></label>
+      <button id="add-button">${button}</button>
+      <ul id="todoList"></ul>
     </div>
   `;
+  const addButton = document.getElementById("add-button");
+  const todoInput = document.getElementById("todo-input");
+  const todoList = document.getElementById("todoList");
+
+  addButton.addEventListener("click", () => {
+    const todoText = todoInput.value.trim();
+    todoList.innerHTML += `<li>${todoText}</li>`;
+    todoInput.value = "";
+  });
+  todoList.addEventListener("click", (e) => {
+    if (e.target.tagName === "li") {
+      e.target.classList.toggle("completed");
+    }
+  });
 };
-todo();
+todo("ADD");
