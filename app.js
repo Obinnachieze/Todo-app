3261555267edbb78cfad3e2a798b43f7a72a7af4
Obinnachieze <wisdomchieze0@gmail.com>
2026-01-14 21:55:58 +0100

    err

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 try {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  if (!response.ok) {
-    throw new Error("this message is not here");
-  }
-  const data = await response.json();
+  const get = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
 
-  const image = document.createElement("img");
-  const para = document.createElement("p");
-  image.src = data.message;
-  image.alt = "my new dog";
-  para.innerHTML = data.status;
-  document.body.appendChild(image);
-  document.body.appendChild(para);
-} catch (error) {}
+    const data = await response.json();
+
+    const image = document.createElement("img");
+    const para = document.createElement("p");
+    image.src = data.message;
+    image.alt = "my new dog";
+    para.innerHTML = data.status;
+    document.body.appendChild(image);
+    document.body.appendChild(para);
+  };
+  get();
+} catch (error) {
+  console.log("error", error);
+}
